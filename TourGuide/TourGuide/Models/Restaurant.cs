45666c3a656d1594e8a4168bd39894fccc5e851b
gar1 <garvanpax@aol.com>
2014-08-05 11:00:12 -0400

    Updated Layout.cshtml

--- TourGuide/TourGuide/Models/Restaurant.cs
+++ TourGuide/TourGuide/Models/Restaurant.cs
@@ -11,6 +11,7 @@ namespace TourGuide.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
     
     public partial class Restaurant
     {
@@ -18,11 +19,15 @@ namespace TourGuide.Models
         public int CityID { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
+        [Display(Name="Zip Code")]
         public Nullable<int> ZipCode { get; set; }
+        [Display(Name = "Phone Number")]
         public string Phone { get; set; }
         public string URL { get; set; }
         public Nullable<int> Rating { get; set; }
+        [Display(Name = "Latitude")]
         public Nullable<decimal> Lat { get; set; }
+        [Display(Name = "Longitude")]
         public Nullable<decimal> Long { get; set; }
     
         public virtual Location Location { get; set; }
